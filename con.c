fb859ff30a23835a80603d619ce0e6feb7757a1a
Astra <richardakuzor1@gmail.com>
2026-01-06 19:55:05 +0100

    Add files via upload

--- con.c
+++ con.c
@@ -1,17 +1,12 @@
 #include<stdio.h>
 int main(void) //simple condutional program i learmt today 
 {
-  int x = 0;
+  int x = 50;
   printf("Enter a number: ");
   scanf("numb: %d\n", &x);
   if (x == 50)
   {
     printf("weldone,you won!\n");
-  }
-  else if (x == 10)
-  {
-    printf("try more time!\n");
-
   }
   else {
     printf("try again!\n");

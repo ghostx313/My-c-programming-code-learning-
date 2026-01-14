1d016114c101eb36c5ca3611251b94d219e448d5
Astra <richardakuzor1@gmail.com>
2026-01-14 10:39:48 +0100

    Create Evennorodd.c

---
+++ Evennorodd.c
@@ -0,0 +1,28 @@
+// even or odd 
+
+#include<stdio.h>
+
+int main(void)
+{
+	int number;
+	int time;
+	
+	time = 7;
+	number = 6;
+  
+
+	
+	
+	
+	if (number > time)
+	{
+		printf("number is greater");
+		
+	}
+	else
+	{
+		printf("it is small ");
+	}
+	
+	return 0;
+}

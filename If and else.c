5a0ee63ce9634c9679b8824c3e0ec14cb53821f7
Astra <richardakuzor1@gmail.com>
2026-01-14 10:22:29 +0100

    Statement in c

---
+++ If and else.c
@@ -0,0 +1,20 @@
+// if and else pratice in c 
+
+#include<stdio.h>
+
+int main(void)
+{
+	int number = 0;
+	printf("Enter a number: ");
+	scanf("number: %d", &number);
+	if (number % 2 == 0)
+	{
+		printf("even\n");
+	}
+	else if (number % 2 )
+	{
+		printf("odd\n");
+	}
+	
+	return 0;
+}
